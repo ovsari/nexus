0b8a62762f2da5eef8d1285975203b9757b58046
OVIYA A <91796842+ovsari@users.noreply.github.com>
2024-01-29 17:55:14 +0530

    project2

---
+++ script.js
@@ -0,0 +1,8 @@
+var navbar = document.querySelector(".navbar");
+var sidenav = document.querySelector(".side-nav");
+function openNav(){
+    sidenav.style.left="0";
+}
+function closeNav(){
+    sidenav.style.left="-60%";
+}
