ddd61b14d76908dc27e6b78794921a196d2e3925
OVIYA <oviyaprabavathy@gmail.com>
2024-01-29 18:22:13 +0530

    project2

--- script.js
+++
@@ -1,8 +0,0 @@
-var navbar = document.querySelector(".navbar");
-var sidenav = document.querySelector(".side-nav");
-function openNav(){
-    sidenav.style.left="0";
-}
-function closeNav(){
-    sidenav.style.left="-60%";
-}
